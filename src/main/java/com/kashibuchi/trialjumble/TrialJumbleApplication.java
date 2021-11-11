ef5fa2b832ef25f1a89e75e5c29c20ddc5f5a545
KashibuchiKyamin <y.mrkm.0722@gmail.com>
2021-11-11 23:31:46 +0900

    初回コミット

---
+++ src/main/java/com/kashibuchi/trialjumble/TrialJumbleApplication.java
@@ -0,0 +1,13 @@
+package com.kashibuchi.trialjumble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialJumbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialJumbleApplication.class, args);
+	}
+
+}
