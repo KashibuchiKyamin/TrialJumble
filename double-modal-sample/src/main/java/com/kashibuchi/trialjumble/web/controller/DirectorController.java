66228bc19022e2b4b05943103125738dad01b49f
KashibuchiKyamin <y.mrkm.0722@gmail.com>
2021-11-14 14:55:07 +0900

    コミット漏れ分の追加

---
+++ double-modal-sample/src/main/java/com/kashibuchi/trialjumble/web/controller/DirectorController.java
@@ -0,0 +1,32 @@
+package com.kashibuchi.trialjumble.web.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kashibuchi.trialjumble.service.DirectorWorksService;
+import com.kashibuchi.trialjumble.web.model.DirectorWorks;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class DirectorController {
+
+	static private Logger LOGGER = LogManager.getLogger(DirectorController.class);
+
+	private DirectorWorksService directorWorksService;
+
+	@GetMapping("/directorWorks/{directorId}")
+	public DirectorWorks getData(@PathVariable("directorId") String directorId) {
+
+		DirectorWorks response = directorWorksService.getDirectorWorksDetail(directorId);
+
+		LOGGER.debug("response: {}", response);
+
+		return response;
+	}
+
+}
