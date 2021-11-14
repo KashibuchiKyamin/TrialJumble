51c51af8e526597cd9718fb22175504bb88d3de1
KashibuchiKyamin <y.mrkm.0722@gmail.com>
2021-11-14 12:28:55 +0900

    ひとまず、ソース一式のディレクトリ構成を変更。

--- rest-api-sample/src/main/java/com/kashibuchi/trialjumble/RestApiSampleApplication.java
+++ rest-api-sample/src/main/java/com/kashibuchi/trialjumble/RestApiSampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrialJumbleApplication {
+public class RestApiSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrialJumbleApplication.class, args);
+		SpringApplication.run(RestApiSampleApplication.class, args);
 	}
 
 }
