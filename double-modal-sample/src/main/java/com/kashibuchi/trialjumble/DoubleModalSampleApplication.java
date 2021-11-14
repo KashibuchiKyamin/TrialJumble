548d9df035e39a1bf13c41839bc8304c70b5c528
KashibuchiKyamin <y.mrkm.0722@gmail.com>
2021-11-14 14:47:51 +0900

    初回コミット

---
+++ double-modal-sample/src/main/java/com/kashibuchi/trialjumble/DoubleModalSampleApplication.java
@@ -0,0 +1,13 @@
+package com.kashibuchi.trialjumble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleModalSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleModalSampleApplication.class, args);
+	}
+
+}
