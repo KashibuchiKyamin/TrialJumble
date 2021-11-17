ad0b60224d4ac5504e85dcd63d7d7b04b0063b85
KashibuchiKyamin <y.mrkm.0722@gmail.com>
2021-11-17 23:06:22 +0900

    初回コミット(JUnitのパターンづくり)

---
+++ junit-demo/src/test/java/com/kashibuchi/trialjumble/JunitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashibuchi.trialjumble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
