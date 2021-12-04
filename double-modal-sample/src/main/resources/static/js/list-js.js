f45e82271ff8fed67c0c469ed20fadc1d2c8a564
KashibuchiKyamin <y.mrkm.0722@gmail.com>
2021-12-04 13:18:39 +0900

    MicroModal.show()ってこう使うのね。的な。

--- double-modal-sample/src/main/resources/static/js/list-js.js
+++ double-modal-sample/src/main/resources/static/js/list-js.js
@@ -1,6 +1,18 @@
+var data = document.getElementById('target');
+console.log(data);
+
 var options = {
 	valueNames: ['workName', 'workTypeJa', 'yearOfRelease'],
-	item: '<tr><td class="workName"></td><td class="workTypeJa"></td><td class="yearOfRelease"></td></tr>',
+	//item: '<tr><td class="workName"></td><td class="workTypeJa"></td><td class="yearOfRelease"></td></tr>',
+
+	item: function(values) {
+		return `<tr>
+				<td>${values.workName}</td>
+				<td>${values.workTypeJa}</td>
+				<td>${values.yearOfRelease}<button onclick="openDiv('${values}')">押してね</button></td>
+				</tr>`;
+	},
+
 	page: 5,
 	pagination: true,
 	innerWindow: 2,
